ee777a4a26173f115451659ad1a1ef2d6b819a4f
LucaFerar <luca.ferartofan@studio.unibo.it>
2026-04-20 11:33:04 +0200

    feat: added support for multiple languages(java and scala)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,20 @@
-plugins { java }
+plugins {
+    java
+    scala
+}
+
+tasks.withType<ScalaCompile>().configureEach {
+    scalaCompileOptions.additionalParameters = listOf(
+        "-Xunchecked-java-output-version",
+        JavaVersion.current().majorVersion,
+    )
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val scalaVersion = "3.8.3"
+    implementation("org.scala-lang:scala3-library_3:${scalaVersion}")
+}
---
+++ src/main/scala/BrickOwens.scala
@@ -0,0 +1,2 @@
+class BrickOwens:
+  val brick = new Brick(10, 20)
